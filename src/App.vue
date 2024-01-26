8da877ed1153ff3b6e503870ae2a2fe1026255d3
davidequattrocchi10 <davidequattrocchi10@gmail.com>
2024-01-26 16:07:15 +0100

    First commit in vite

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "App",
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      Hello World
+    </h1>
+  </header>
+  <main></main>
+  <footer></footer>
+</template>
+
+<style ></style>
